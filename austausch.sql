a0d6bf017de610d6a7d1efa5b39602d69f524d51
Tobias Pichler <tobias.pichler@edu.campus02.at>
2018-06-09 09:09:02 +0200

    Update austausch.sql

--- austausch.sql
+++ austausch.sql
@@ -1,3 +1,9 @@
 Bitte hier die Kommunikation eintragen und committen:
 
-test
+
+1)
+select c.companyname as [Customer], count(distinct o.orderid) as Orders, count(distinct od.productid) as Products from Sales.Customers c
+join Sales.Orders o on o.custid = c.custid
+join Sales.OrderDetails od on o.orderid = od.orderid
+group by c.companyname
+order by [Customer]
