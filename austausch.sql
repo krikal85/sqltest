c611a5c6909950c0a6ff020403c42ca76ec9c83d
Tobias Pichler <tobias.pichler@edu.campus02.at>
2018-06-09 09:20:56 +0200

    Update austausch.sql

--- austausch.sql
+++ austausch.sql
@@ -7,3 +7,20 @@ join Sales.Orders o on o.custid = c.custid
 join Sales.OrderDetails od on o.orderid = od.orderid
 group by c.companyname
 order by [Customer]
+
+2)
+GO
+create function Sales.udf_DistinctOrdersAndProductsCountPerCustomerFiltered(@MinOrders int, @MaxOrders int, @MinProducts int, @MaxProducts int)
+returns table
+as
+return
+(
+	select c.companyname as [Customer], count(distinct o.orderid) as Orders, count(distinct od.productid) as Products from Sales.Customers c
+	join Sales.Orders o on o.custid = c.custid
+	join Sales.OrderDetails od on o.orderid = od.orderid
+	group by c.companyname
+	having count(distinct o.orderid) > @MinOrders AND count(distinct o.orderid) < @MaxOrders AND count(distinct od.productid) > @MinProducts AND count(distinct od.productid) < @MaxProducts
+);
+
+GO
+select * from Sales.udf_DistinctOrdersAndProductsCountPerCustomerFiltered (1,10,1,30);
