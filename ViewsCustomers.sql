b324d6cec8595f52c465d8f606610f261637a2b7
zlatko42 <zlatko.knezevic@edu.campus02.at>
2018-06-20 21:40:53 +0200

    Delete ViewsCustomers.sql

--- ViewsCustomers.sql
+++
@@ -1,6 +0,0 @@
-Drop View uvw_Top10Customers
-Create View uvw_Top10Customers AS
-select TOP 10 c.companyname, c.custid, COUNT(c.custid) as AmountOrdersFromCustomers, YEAR(o.orderdate) AS JAHR from [Sales].[Orders] as o
-join Sales.Customers as c on c.custid = o.custid
-group by c.custid, c.companyname, YEAR(o.orderdate)
-order by AmountOrdersFromCustomers DESC
