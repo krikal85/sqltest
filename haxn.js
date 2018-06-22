01bc9fd587a9064628c84b2e709f4560fd6610f4
zlatko42 <zlatko.knezevic@edu.campus02.at>
2018-06-22 15:17:06 +0200

    Update haxn.js

--- haxn.js
+++ haxn.js
@@ -1,3 +1,109 @@
+//zlatko:
+// ------------------------
+// -- logic to implement --
+// ------------------------
+
+var rechnungen = [];
+
+function loadOverview() {
+	
+    var url = "http://oliverklemencic.com/campus/rechnungen.json";
+    loadAjax(url, function(ajaxText){
+        data = JSON.parse(ajaxText);
+
+        for(var i = 0; i < data.length; i++){
+            addRechnungToTable(data[i]);
+        }
+    })
+    
+}	
+
+function addRechnungToTable(rechnung) {
+    var body = document.getElementById("rechnungenBody");
+    var reihe = document.createElement("tr");
+    
+    for(spalte in rechnung){
+        var td = document.createElement("td");
+        td.innerText = rechnung[spalte];
+        reihe.appendChild(td);
+    }
+    var td = document.createElement("td");
+    var btn = document.createElement("button");
+    btn.innerText="Details";
+    btn.setAttribute("id", rechnung["nummer"])
+    btn.addEventListener("click", function(e) {
+        showDetailsForRechnung(e.target.id);});
+        td.appendChild(btn);
+        reihe.appendChild(td);
+        body.appendChild(reihe);
+        rechnungen.push(rechnung);
+        
+}
+
+function showDetailsForRechnung(rechnung) {
+	
+    var url = "http://oliverklemencic.com/campus/details-" + rechnung + ".json";
+    loadAjax(url,function(ajaxText){
+        data = JSON.parse(ajaxText);
+        var name = document.getElementById("detailsName");
+        name.value = data["beschreibung"];
+
+        var preis = document.getElementById("detailsPrice");
+        preis.value = data["summe"];
+        
+    });
+    
+}
+
+function registerForSearch() {
+	
+}
+
+function search(searchTerm) {
+	
+}
+
+
+// --------------------
+// -- initialization --
+// --------------------
+
+registerForSearch();
+loadOverview();
+
+
+// ----------------------
+// -- helper functions --
+// ----------------------
+
+function loadAjax(url, callback) {
+	var xhr=new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        // Check for a completed connection
+        if (xhr.readyState==4)
+        {
+            if (xhr.status==200)
+            {
+				callback(xhr.responseText);
+				
+            }
+            else
+            {
+                alert("Problem retrieving data!");
+            }
+        }
+    };
+	xhr.open("GET", url, true);
+	xhr.send("");
+}
+//
+//
+//
+//
+
+
+
+
 //hier bitte die lösungen rein
 //von gollner es geht alles außer e
 // ------------------------
